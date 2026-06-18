3f31ba881300d4e918a1be2b6d2425387fb6e155
Carla Prado <ca.pradom@duocuc.cl>
2026-06-18 03:53:45 -0400

    config para deploy y no local

--- src/api/inscripcionApiRequest.js
+++ src/api/inscripcionApiRequest.js
@@ -1,4 +1,4 @@
-const BASE = "http://localhost:8080";
+const BASE = import.meta.env.VITE_API_URL;
 
 const headers = () => {
     const usuario = JSON.parse(sessionStorage.getItem("usuario") || "{}");
