5b56fc38b8c8af6257f57f36324146d493eff42e
Carla Prado <ca.pradom@duocuc.cl>
2026-06-18 04:00:49 -0400

    test

--- src/screens/login/LoginScreen.jsx
+++ src/screens/login/LoginScreen.jsx
@@ -15,6 +15,7 @@ export default function LoginScreen() {
   const handleLogin = async (e) => {
     e.preventDefault();
 
+    // comentario
     try {
       const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
         method: "POST",
